0e82452597928f02bd31d3163ec0c702d6972966
doosemavis <moosedavis2011@gmail.com>
2020-10-17 13:24:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -8,12 +8,20 @@ module FormsLab
       erb :root
     end 
 
-    get '/new' do 
-
+    get '/new' do
+      erb :'pirates/new'
     end 
 
     post '/pirates' do 
+      @pirate = Pirate.new(params[:pirate])
 
+      params[:pirate][:ships].each do |details|
+        Ship.new(deatils)
+      end 
+
+      @ships = Ship.all
+      
+      erb :'pirates/show'
     end 
 
   end
