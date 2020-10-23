56e4a6c1f8d18cf705a306b6adc6938453ecb820
cailinkless <cailin.nysff@gmail.com>
2020-10-23 12:46:45 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+    attr_accessor :name, :height, :weight
+
+    def initialize(params)
+        @name = params[:name]
+        @height = params[:height]
+        @weight = params[:weight]
+    end
+    
 end
